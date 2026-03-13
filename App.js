6ef41a7a147f2f1aaea046a0d8f5eb99bf6898b4
Juan Pablo Perez <juanpabloperezmartin0@gmail.com>
2026-03-12 21:24:28 -0600

    asd

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './components/Home';
+import Info from './components/Info';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Página de Inicio' }} />
+        <Stack.Screen name="Info" component={Info} options={{ title: 'Información' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
